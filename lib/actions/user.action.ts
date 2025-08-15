1e3d08474f55b98844bc500ce21de3987ffc4be6
Iamloumie <lawaladedamolaolumide@gmail.com>
2025-08-15 09:48:32 -0700

    old packages

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,95 +1,81 @@
-// This is a NEXTJS server action that creates a user
-'use server';
+'use server'
 
-import { CreateUserParams, UpdateUserParams } from "@/types";
-import { connectToDatabase } from "../mongodb/database";
-import User from "../mongodb/database/models/user.model";
-import Order from "../mongodb/database/models/order.model";
-import Event from "../mongodb/database/models/event.model";
-import { handleError } from "../utils";
-import { revalidatePath } from "next/cache";
+import { revalidatePath } from 'next/cache'
 
-// This function creates a new user in the database
-export const createUser = async (user: CreateUserParams) => {
-    try {
-        await connectToDatabase();
+import { connectToDatabase } from '@/lib/database'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
+import { handleError } from '@/lib/utils'
 
-        // For Mongoose 8.x, be explicit with the create method
-        const newUser = await User.create(user);
+import { CreateUserParams, UpdateUserParams } from '@/types'
 
-        return JSON.parse(JSON.stringify(newUser));
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase()
 
-    } catch (error) {
-        handleError(error);
-    } 
+    const newUser = await User.create(user)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-// THIS FUNCTION GETS USER ID  
 export async function getUserById(userId: string) {
-    try {
-        await connectToDatabase();
+  try {
+    await connectToDatabase()
 
-        // Use exec() for better type inference in Mongoose 8.x
-        const user = await User.findById(userId).exec();
+    const user = await User.findById(userId)
 
-        if (!user) throw new Error('User not found');
-        
-        return JSON.parse(JSON.stringify(user));
-    } catch (error) {
-        handleError(error);
-    }
+    if (!user) throw new Error('User not found')
+    return JSON.parse(JSON.stringify(user))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-// THIS FUNCTION UPDATES THE USER
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
-    try {
-        await connectToDatabase();
+  try {
+    await connectToDatabase()
 
-        const updatedUser = await User.findOneAndUpdate(
-            { clerkId }, 
-            user, 
-            { new: true }
-        ).exec();
-        
-        if (!updatedUser) throw new Error('User update failed');
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
 
-        return JSON.parse(JSON.stringify(updatedUser));
-    } catch (error) {
-        handleError(error);
-    }
+    if (!updatedUser) throw new Error('User update failed')
+    return JSON.parse(JSON.stringify(updatedUser))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-// THIS FUNCTION DELETES THE USER BY ID
 export async function deleteUser(clerkId: string) {
-    try {
-        await connectToDatabase();
+  try {
+    await connectToDatabase()
 
-        const userToDelete = await User.findOne({ clerkId }).exec();
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId })
 
-        if (!userToDelete) throw new Error('User not found');
-
-        // unlink relationships
-        await Promise.all([
-            // update the 'events' collection to remove references to the user
-            Event.updateMany(
-                { _id: { $in: userToDelete.events || [] } },
-                { $pull: { organizer: userToDelete._id } }
-            ).exec(),
-
-            // update the 'orders' collection to remove references to the user
-            Order.updateMany(
-                { _id: { $in: userToDelete.orders || [] } },
-                { $unset: { buyer: 1 } }
-            ).exec()
-        ]);
-
-        // delete the user
-        const deletedUser = await User.findByIdAndDelete(userToDelete._id).exec();
-        revalidatePath('/');
-
-        return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-
-    } catch (error) {
-        handleError(error);
+    if (!userToDelete) {
+      throw new Error('User not found')
     }
+
+    // Unlink relationships
+    await Promise.all([
+      // Update the 'events' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+    ])
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+    revalidatePath('/')
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+  } catch (error) {
+    handleError(error)
+  }
 }
